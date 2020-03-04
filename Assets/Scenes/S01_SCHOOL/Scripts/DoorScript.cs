e45adcad54325a5aa248df7ba6b09d20ca1e5580
Alec Bell <alec.timothy.bell@gmail.com>
2020-03-04 18:54:56 -0500

    Minor cleanup/bug fixes

--- Assets/Scenes/S01_SCHOOL/Scripts/DoorScript.cs
+++ Assets/Scenes/S01_SCHOOL/Scripts/DoorScript.cs
@@ -10,7 +10,7 @@ public class DoorScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        close = true;
+        close = false;
 		currentRotation = 10.0f;
     }
 
