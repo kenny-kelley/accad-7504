54eeea255309434e83d78401449cd82050fb53b1
Bell <alec.timothy.bell@gmail.com>
2020-03-04 15:55:32 -0500

    Removed static property from door

--- Assets/Scenes/S01_SCHOOL/Scripts/DoorScript.cs
+++ Assets/Scenes/S01_SCHOOL/Scripts/DoorScript.cs
@@ -10,7 +10,7 @@ public class DoorScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        close = false;
+        close = true;
 		currentRotation = 10.0f;
     }
 
