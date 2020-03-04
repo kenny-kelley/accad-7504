32b16e058b7ed46f5b547d16a0b4ea2f406e4f9f
Bell <alec.timothy.bell@gmail.com>
2020-03-04 15:48:42 -0500

    Close door during attack

--- Assets/Scenes/S01_SCHOOL/Scripts/DoorScript.cs
+++ Assets/Scenes/S01_SCHOOL/Scripts/DoorScript.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
 	private bool close;
+	private float currentRotation;
 	
     // Start is called before the first frame update
     void Start()
     {
         close = false;
+		currentRotation = 10.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-		float rotateSpeed = 0.0f;
-        if (close)
-			rotateSpeed = 6.0f;
-		transform.Rotate(0, rotateSpeed, 0, Space.Self);
+        if (close && currentRotation < 90.0f)
+		{
+			float rotateSpeed = 5.0f;
+			transform.Rotate(0, rotateSpeed, 0, Space.Self);
+			currentRotation += rotateSpeed;
+		}
     }
 	
 	public void Close()
